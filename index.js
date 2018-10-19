2f24d5661bfb306b0124e29be04b6a5728223df1
koverko-developer <kowerkodeveloper@gmail.com>
2018-10-20 01:15:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 'use strict'
 
 var app = require('express')(),
-    http = require('http').createServer(app),
-    io = require('socket.io')(http),
-    port = process.env.PORT || 80,
-    publicDir = `${__dirname}/public`
-
+	http = require('http').createServer(app),
+	io = require('socket.io')(http),
+	port = process.env.PORT || 80,
+	publicDir = `${__dirname}/public`
 
 http.listen(port, () => {
-    console.log('localhost: ', port);
+	console.log('Iniciando Express y Socket.IO en localhost:%d', port)
 })
 
 app
-    //.use(publicDir)
-    .get('/', (req, res) =>{
-      res.sendFile(`${publicDir}/client.html`)
-    })
-    .get('/1111/streaming', (req, res) => {
-      res.sendFile(`${publicDir}/server.html`)
-      io.on('connection', (socket) =>{
-          socket.on('streaming', (image) =>{
-              io.emit('play stream 1', image)
-              //console.log(image);
-          })
-      })
-    })
-    
+	.get('/', (req, res) => {
+		res.sendFile(`${publicDir}/client.html`)
+	})
+	.get('/streaming', (req, res) => {
+		res.sendFile(`${publicDir}/server.html`)		
+	})
+
+io.on('connection', (socket) => {
+	socket.on('streaming', (image) => {
+		io.emit('play stream', image)
+		//console.log(image)
+	})	
+})
